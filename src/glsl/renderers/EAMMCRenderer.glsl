f4979dcc4787f64065a6abbc70ebd932c0899232
vkriznar <krizovid@gmail.com>
2021-04-27 17:58:49 +0200

    Update EAMMCRenderer.glsl

--- src/glsl/renderers/EAMMCRenderer.glsl
+++ src/glsl/renderers/EAMMCRenderer.glsl
@@ -97,30 +97,20 @@ vec3 randomDirection(vec2 U) {
 }
 
 void main() {
-    // Create a Photon object with position, direction, transmittance, radience, bounces and samples
     Photon photon; 
-    // Create 2d vector mapped position from vPosition (what is vPosisition - produces by GPU?) - YES
     vec2 mappedPosition = vPosition * 0.5 + 0.5;
-    // What does texture do? uPosition?
     photon.position = texture(uPosition, mappedPosition).xyz;
-    // xyz attribute takes care of direction of photon bounce, based on position, w?
     vec4 directionAndBounces = texture(uDirection, mappedPosition);
     photon.direction = directionAndBounces.xyz;
     photon.bounces = uint(directionAndBounces.w + 0.5);
-    // Set photon's trasmitance to mapped position
     photon.transmittance = texture(uTransmittance, mappedPosition).rgb;
     vec4 radianceAndSamples = texture(uRadiance, mappedPosition);
-    // Set photon's radiance based on uRadiance and position, w?
     photon.radiance = radianceAndSamples.rgb;
     photon.samples = uint(radianceAndSamples.w + 0.5);
 
-    // Create new random seed in the form of vec2 r, what is r.x what is r.y? Does that just mean we
-    // generate two seperate random floats from [0, 1)?
     vec2 r = rand(vPosition * uRandSeed);
     for (uint i = 0u; i < uSteps; i++) {
         r = rand(r);
-        // TODO: Calculate step size based on exponential distance travelled and uMajorant, what is uMajorant?
-        // Is uMajorant like a normalizing factor?
         float t = -log(r.x) / uMajorant;
         photon.position += t * photon.direction;
 
@@ -130,46 +120,29 @@ void main() {
         float muNull = uMajorant - muAbsorption - muEmission;
         float muMajorant = muAbsorption + muEmission + abs(muNull);
         float PNull = abs(muNull) / muMajorant;
-        // TODO: PAbsorption based on a value of volume and absorption coefficient we then normalize it?
         float PAbsorption = muAbsorption / muMajorant;
         float PEmission = muEmission / muMajorant;
 
         if (any(greaterThan(photon.position, vec3(1))) || any(lessThan(photon.position, vec3(0)))) {
-            // out of bounds
             vec3 radiance = vec3(0);
             photon.samples++;
             photon.radiance += (radiance - photon.radiance) / float(photon.samples);
-            // What does resetting a photon do if we're out of bounce
             resetPhoton(r, photon);
         } else if (photon.bounces >= uMaxBounces) {
-            // max bounces achieved -> only estimate transmittance
-            // What is weightAS?
             float weightAS = (muAbsorption + muEmission) / uMajorant;
             photon.transmittance *= 1.0 - weightAS;
         } else if (r.y < PAbsorption) {
-            // absorption - What is weightA? How much of our photon gets absorbed?
             float weightA = muAbsorption / (uMajorant * PAbsorption);
             photon.transmittance *= 1.0 - weightA;
         } else if (r.y < PAbsorption + PEmission) {
-            // emission - I intend to do emission based on muAbsorption, if volume has absorped some
-            // than it is more likely to emmit a photon. In slides funamentals page 7 there is 
-            // modeling the emission as abs_coeff * emitted_radiance, what is emmited radience?
-            // Here we generate a new Proton most likely, which means we now have two photons,
-            // what do we need to call for the integrate part to be done on the new photon? Also
-            // how much transmittance does the new photon have, same as volume had muAbsorption?
             vec3 radiance = photon.transmittance * volumeSample.rgb;
-            radiance += photon.radiance
+            radiance += photon.radiance;
         } else {
-            // null collision - what is null collision, photon just passing through? Or is it
-            // photon colliding with fictitious medium?
-            // What is weightN?
             float weightN = muNull / (uMajorant * PNull);
-            // Why do we lower transmittance
             photon.transmittance *= weightN;
         }
     }
 
-    // What are these oValues? original photon values?
     oPosition = vec4(photon.position, 0);
     oDirection = vec4(photon.direction, float(photon.bounces));
     oTransmittance = vec4(photon.transmittance, 0);
@@ -242,7 +215,6 @@ void main() {
     Photon photon;
     vec3 from, to;
     vec2 randState = rand(vPosition * uRandSeed);
-    // TODO: What does unprojectRand do?
     unprojectRand(randState, vPosition, uMvpInverseMatrix, uInverseResolution, uBlur, from, to);
     photon.direction = normalize(to - from);
     vec2 tbounds = max(intersectCube(from, photon.direction), 0.0);
